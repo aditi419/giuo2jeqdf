1b0f076177a0aeb68c1d936565b1225ee2f97ec9
aditi419 <76972940+aditi419@users.noreply.github.com>
2021-07-01 20:49:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+music1 = "";
+music2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+leftWrist_score = 0;
+song1_status = "";
+song2_statue = "";
+
+function preload(){
+    music1 = loadSound("music.mp3");
+    music2 = loadSound("music2.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
+}
+
+function draw(){
+    image(video,0,0,600,500);
+
+    fill("#FF0000");
+    stroke("FF0000");
+
+    music1.isPlaying();
+
+    if(leftWrist_score > 0.2){
+        circle(leftWristX,leftWristY,20);
+
+        music2.stop();
+    }
+
+    if(song1_status == false){
+        music1.play();
+        
+        document.getElementById("songName").innerHTML = music1;
+    }
+}
